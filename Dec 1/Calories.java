f2760b55326c0f6b32d9ac193fde78cf6d592835
arremiasz <arremiasz@stu.naperville203.org>
2022-12-03 00:35:39 -0600

    Tabs -> Spaces

--- Dec 1/Calories.java
+++ Dec 1/Calories.java
@@ -53,60 +53,60 @@ import java.util.Scanner;
 
 public class Calories {
 
-	public static void main(String[] args) throws FileNotFoundException {
-		part1();
-		part2();
-	}
-	
-	public static void part1() throws FileNotFoundException {
-		File file = new File("/Users/aremiasz/Documents/GitHub/Advent of Code 2022/Dec 1/input.txt");
-		Scanner in = new Scanner(file);
-		int biggest = 0;
-		while (in.hasNextLine()) {
-			int sum = 0;
-			String n = in.nextLine();
-			do {
-				sum += Integer.parseInt(n);
-				if (in.hasNextLine())
-					n = in.nextLine();
-				else break;
-			}
-			while (!n.equals(""));
-			if (sum > biggest) biggest = sum;
-		}
-		in.close();
-		System.out.println(biggest);
-	}
-	
-	public static void part2() throws FileNotFoundException {
-		File file = new File("/Users/aremiasz/Documents/GitHub/Advent of Code 2022/Dec 1/input.txt");
-		Scanner in = new Scanner(file);
-		int biggest = 0;
-		int second = 0;
-		int third = 0;
-		ArrayList<Integer> list = new ArrayList<>();
-		while (in.hasNextLine()) {
-			int sum = 0;
-			String n = in.nextLine();
-			do {
-				sum += Integer.parseInt(n);
-				if (in.hasNextLine())
-					n = in.nextLine();
-				else break;
-			}
-			while (!n.equals(""));
-			list.add(sum);
-			if (sum > biggest) biggest = sum;
-			else if (sum > second) second = sum;
-			else if (sum > third) third = sum;
-		}
-		list.sort(null);
-		biggest = list.get(list.size()-1);
-		second = list.get(list.size()-2);
-		third = list.get(list.size()-3);
-		
-		in.close();
-		System.out.print(biggest+second+third);
-	}
+    public static void main(String[] args) throws FileNotFoundException {
+        part1();
+        part2();
+    }
+
+    public static void part1() throws FileNotFoundException {
+        File file = new File("/Users/aremiasz/Documents/GitHub/Advent of Code 2022/Dec 1/input.txt");
+        Scanner in = new Scanner(file);
+        int biggest = 0;
+        while (in.hasNextLine()) {
+            int sum = 0;
+            String n = in.nextLine();
+            do {
+                sum += Integer.parseInt(n);
+                if (in.hasNextLine())
+                    n = in.nextLine();
+                else break;
+            }
+            while (!n.equals(""));
+            if (sum > biggest) biggest = sum;
+        }
+        in.close();
+        System.out.println(biggest);
+    }
+
+    public static void part2() throws FileNotFoundException {
+        File file = new File("/Users/aremiasz/Documents/GitHub/Advent of Code 2022/Dec 1/input.txt");
+        Scanner in = new Scanner(file);
+        int biggest = 0;
+        int second = 0;
+        int third = 0;
+        ArrayList<Integer> list = new ArrayList<>();
+        while (in.hasNextLine()) {
+            int sum = 0;
+            String n = in.nextLine();
+            do {
+                sum += Integer.parseInt(n);
+                if (in.hasNextLine())
+                    n = in.nextLine();
+                else break;
+            }
+            while (!n.equals(""));
+            list.add(sum);
+            if (sum > biggest) biggest = sum;
+            else if (sum > second) second = sum;
+            else if (sum > third) third = sum;
+        }
+        list.sort(null);
+        biggest = list.get(list.size()-1);
+        second = list.get(list.size()-2);
+        third = list.get(list.size()-3);
+
+        in.close();
+        System.out.print(biggest+second+third);
+    }
 
 }
--- Dec 2/RockPaperScissors.java
+++ Dec 2/RockPaperScissors.java
@@ -37,95 +37,95 @@ In the third round, you will defeat your opponent's Scissors with Rock for a sco
 Now that you're correctly decrypting the ultra top secret strategy guide, you would get a total score of 12.
 
 Following the Elf's instructions for the second column, what would your total score be if everything goes exactly according to your strategy guide?
-*/
+ */
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class RockPaperScissors {
 
-	public static void main(String[] args) throws FileNotFoundException{
-		File file = new File("/Users/aremiasz/Documents/GitHub/Advent of Code 2022/Dec 2/input.txt");
-		part1(file);
-		part2(file);
-	}
+    public static void main(String[] args) throws FileNotFoundException{
+        File file = new File("/Users/aremiasz/Documents/GitHub/Advent of Code 2022/Dec 2/input.txt");
+        part1(file);
+        part2(file);
+    }
 
-	public static void part1(File file) throws FileNotFoundException {
-		Scanner in = new Scanner(file);
-		int score = 0;
-		while (in.hasNext()) {
-			int round = 0;
-			String opponent = in.next();
-			String you = in.next();
-			// if opponent is rock
-			if (opponent.equals("A")) {
-				// rock + rock
-				if (you.equals("X")) round += 1+3;
-				// rock + paper
-				else if (you.equals("Y")) round += 2+6;
-				// rock + scissors
-				else if (you.equals("Z")) round += 3;
-			} // if opponent is paper
-			else if (opponent.equals("B")) {
-				// paper + rock
-				if (you.equals("X")) round += 1;
-				// paper + paper
-				else if (you.equals("Y")) round += 2+3;
-				// paper + scissors
-				else if (you.equals("Z")) round += 3+6;
-			} // if opponent is scissors
-			else if (opponent.equals("C")) {
-				// scissors + rock
-				if (you.equals("X")) round += 1+6;
-				// scissors + paper
-				else if (you.equals("Y")) round += 2;
-				// scissors + scissors
-				else if (you.equals("Z")) round += 3+3;
-			}
-			
-			score += round;
-		}
-		in.close();
-		System.out.println(score);
-	}
-	
-	
-	public static void part2(File file) throws FileNotFoundException {
-		Scanner in = new Scanner(file);
-		int score = 0;
-		while (in.hasNext()) {
-			int round = 0;
-			String opponent = in.next();
-			String you = in.next();
-			// if opponent is rock
-			if (opponent.equals("A")) {
-				// you need to lose choose scissors
-				if (you.equals("X")) round += 3;
-				// you need to draw choose rock
-				else if (you.equals("Y")) round += 1+3;
-				// you need to win choose paper
-				else if (you.equals("Z")) round += 2+6;
-			} // if opponent is paper
-			else if (opponent.equals("B")) {
-				// you need to lose choose rock
-				if (you.equals("X")) round += 1;
-				// you need to draw choose paper
-				else if (you.equals("Y")) round += 2+3;
-				// you need to win choose scissors
-				else if (you.equals("Z")) round += 3+6;
-			} // if opponent is scissors
-			else if (opponent.equals("C")) {
-				// you need to lose choose paper
-				if (you.equals("X")) round += 2;
-				// you need to draw choose scissors
-				else if (you.equals("Y")) round += 3+3;
-				// you need to win choose rock
-				else if (you.equals("Z")) round += 1+6;
-			}
-			
-			score += round;
-		}
-		in.close();
-		System.out.println(score);
-	}
+    public static void part1(File file) throws FileNotFoundException {
+        Scanner in = new Scanner(file);
+        int score = 0;
+        while (in.hasNext()) {
+            int round = 0;
+            String opponent = in.next();
+            String you = in.next();
+            // if opponent is rock
+            if (opponent.equals("A")) {
+                // rock + rock
+                if (you.equals("X")) round += 1+3;
+                // rock + paper
+                else if (you.equals("Y")) round += 2+6;
+                // rock + scissors
+                else if (you.equals("Z")) round += 3;
+            } // if opponent is paper
+            else if (opponent.equals("B")) {
+                // paper + rock
+                if (you.equals("X")) round += 1;
+                // paper + paper
+                else if (you.equals("Y")) round += 2+3;
+                // paper + scissors
+                else if (you.equals("Z")) round += 3+6;
+            } // if opponent is scissors
+            else if (opponent.equals("C")) {
+                // scissors + rock
+                if (you.equals("X")) round += 1+6;
+                // scissors + paper
+                else if (you.equals("Y")) round += 2;
+                // scissors + scissors
+                else if (you.equals("Z")) round += 3+3;
+            }
+
+            score += round;
+        }
+        in.close();
+        System.out.println(score);
+    }
+
+
+    public static void part2(File file) throws FileNotFoundException {
+        Scanner in = new Scanner(file);
+        int score = 0;
+        while (in.hasNext()) {
+            int round = 0;
+            String opponent = in.next();
+            String you = in.next();
+            // if opponent is rock
+            if (opponent.equals("A")) {
+                // you need to lose choose scissors
+                if (you.equals("X")) round += 3;
+                // you need to draw choose rock
+                else if (you.equals("Y")) round += 1+3;
+                // you need to win choose paper
+                else if (you.equals("Z")) round += 2+6;
+            } // if opponent is paper
+            else if (opponent.equals("B")) {
+                // you need to lose choose rock
+                if (you.equals("X")) round += 1;
+                // you need to draw choose paper
+                else if (you.equals("Y")) round += 2+3;
+                // you need to win choose scissors
+                else if (you.equals("Z")) round += 3+6;
+            } // if opponent is scissors
+            else if (opponent.equals("C")) {
+                // you need to lose choose paper
+                if (you.equals("X")) round += 2;
+                // you need to draw choose scissors
+                else if (you.equals("Y")) round += 3+3;
+                // you need to win choose rock
+                else if (you.equals("Z")) round += 1+6;
+            }
+
+            score += round;
+        }
+        in.close();
+        System.out.println(score);
+    }
 }
