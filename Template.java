0091a0fe16ea2603efcf81c7baddd789ec5fbd32
arremiasz <arremiasz@stu.naperville203.org>
2022-12-05 01:12:48 -0600

    Create Template.java

---
+++ Template.java
@@ -0,0 +1,19 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class Template {
+
+    public static void main(String[] args) throws Exception {
+        File file = new File("/Users/aremiasz/Documents/GitHub/Advent of Code 2022/Dec #/input.txt");
+        part1(file);
+        part2(file);
+    }
+    
+    public static void part1(File file) throws Exception {
+        Scanner in = new Scanner(file);
+    }
+
+    public static void part2(File file) throws Exception {
+        
+    }
+}
